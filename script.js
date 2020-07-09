faa40504f2c03c12d6bb42fef212a89ebf09d0ef
Glitch (personality-quiz-starter-code) <none>
2020-07-09 16:33:37 +0000

    ✂️🚡 Checkpoint ./script.js:18284927/1046

--- script.js
+++ script.js
@@ -13,35 +13,55 @@ var q1a2 = document.getElementById("q1a2");
 var q1a3 = document.getElementById("q1a3");
 var q1a4 = document.getElementById("q1a4");
 
+var q2a1 = document.getElementById("q2a1");
+var q2a2 = document.getElementById("q2a2");
+var q2a3 = document.getElementById("q2a3");
+var q2a4 = document.getElementById("q2a4");
+
+var q3a1 = document.getElementById("q3a1");
+var q3a2 = document.getElementById("q3a2");
+var q3a3 = document.getElementById("q3a3");
+var q3a4 = document.getElementById("q3a4");
+
 //#TODO: Add Event Listeners to your answer choice variables.
-q1a1.addEventListener("click", result1);
-q1a2.addEventListener("click", result2);
-q1a3.addEventListener("click", result3);
-q1a4.addEventListener("click", result4);
+q1a1.addEventListener("click", avatarAang);
+q1a2.addEventListener("click", southernRaiders);
+q1a3.addEventListener("click", crossroads);
+q1a4.addEventListener("click", tales);
+
+q2a1.addEventListener("click", tales);
+q2a2.addEventListener("click", avatarAang);
+q2a3.addEventListener("click", crossroads);
+q2a4.addEventListener("click", southernRaiders);
+
+q3a1.addEventListener("click", southernRaiders);
+q3a2.addEventListener("click", crossroads);
+q3a3.addEventListener("click", tales);
+q3a4.addEventListener("click", avatarAang);
 
 //#TODO: Define quiz functions here
-function result1() {
+function avatarAang() {
   avatarAangScore += 1;
   questionCount += 1;
   if (questionCount >= 3) {
     updateResult();
   }
 }
-function result2() {
+function southernRaiders() {
   southernRaidersScore += 1;
   questionCount += 1;
   if (questionCount >= 3) {
     updateResult();
   }
 }
-function result3() {
+function crossroads() {
   crossroadsScore += 1;
   questionCount += 1;
   if (questionCount >= 3) {
     updateResult();
   }
 }
-function result4() {
+function tales() {
   talesScore += 1;
   questionCount += 1;
   if (questionCount >= 3) {
